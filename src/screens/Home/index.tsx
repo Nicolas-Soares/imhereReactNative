1d6772e7fa71d79a138d5645e0bd51c7936260cb
Nicolas Soares <nicolas.soares@linkapi.com.br>
2022-08-10 17:42:04 -0300

    initial commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,28 @@
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+
+export default function Home() {
+  function addUser() {
+   console.log("first");
+  }
+
+  return (
+    <View style={styles.viewContainer}>
+      <Text style={styles.firstText}>Evento</Text>
+
+      <Text style={styles.secondText}>Dia 25 de Setembro, 2022</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Digite o seu nome..."
+          placeholderTextColor="#00ff9d58"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={addUser}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  viewContainer: {
+    backgroundColor: "#131016",
+    flex: 1,
+    padding: 24,
+  },
+  firstText: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    paddingTop: 15,
+  },
+  secondText: {
+    color: "#6B6B6B",
+    fontSize: 16,
+  },
+  textInput: {
+    flex: 1,
+    backgroundColor: "#1F1E25",
+    borderRadius: 8,
+    height: 60,
+    padding: 15,
+    color: "#00FF9C",
+    borderWidth: 0.5,
+    marginRight: 10,
+  },
+  buttonText: {
+    color: "#fa61f2",
+    fontSize: 28,
+  },
+  button: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#2c2b33",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 30,
+    marginBottom: 30,
+  },
+});
